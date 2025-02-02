75e518baee8fd632b94437c31ee8a1aad40872c9
Jaden <jadenglenn@pursuit.org>
2025-02-02 10:20:38 -0500

    Initial Commit

---
+++ src/Components/generator.jsx
@@ -0,0 +1,11 @@
+const Generator = () => {
+    return (
+        <>
+        <div>
+            <h1>Generate podcast/information below</h1>
+        </div>
+        </>
+    )
+}
+
+export default Generator
